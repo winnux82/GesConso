1117053e7ee459f3ed9e4371eb609cc8008a7f72
Christophe VANDERMEULEN <christophe.vandermeulen@mouscron.be>
2023-06-19 11:19:14 +0200

    :zap: before commandeArticle modifications :zap:

---
+++ Entities/CommandeArticle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GesConso.Entities
+{
+    public class CommandeArticle
+    {
+        [Key]
+        [Column("Id_Commande")]
+        public Guid? Id_Commande { get; set; }
+
+        [Column("Id_Article")]
+        public Guid? Id_Article { get; set; }
+
+        [Column("Quantite")]
+        public int? Quantite { get; set; }
+
+        [Column("CreatedAt")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Column("UpdatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("DeletedAt")]
+        public DateTime? DeletedAt { get; set; }
+
+
+    }
+
+}
