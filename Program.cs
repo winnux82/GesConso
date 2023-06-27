6dde020fef5afaf54fec815aadc215d502701270
Christophe VANDERMEULEN <christophe.vandermeulen@mouscron.be>
2023-06-27 14:39:52 +0200

    :sparkles: feat loader :sparkles:

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using BlazorStrap;
 using GesConso;
 using GesConso.Data;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,7 @@ builder.Services.AddBlazorStrap();
 //Ajout du service Toast
 builder.Services.AddBlazoredToast();
 builder.Services.AddBlazorise();
+builder.Services.AddSyncfusionBlazor();
 
 
 var app = builder.Build();
@@ -29,6 +31,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
