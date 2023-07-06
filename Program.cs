5861ea6df31cf5fa2beef7fc14126763414ebd70
winnux82 <vandermeulen.christophe@gmail.com>
2023-07-06 15:36:35 +0200

    :zap:  input type datetime-local au lieu de InputDate :zap:

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Blazored.Toast;
 using Blazorise;
 using BlazorStrap;
 using GesConso;
-using GesConso.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Syncfusion.Blazor;
@@ -19,7 +19,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<StateContainer>();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 builder.Services.AddDbContext<Database>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MainConnection")));
 builder.Services.AddBlazorStrap();
 //Ajout du service Toast
